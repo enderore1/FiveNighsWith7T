6715ab3305bae5b8ddb8989d232cfa03b945733a
m0netil <154592830+m0netil@users.noreply.github.com>
2023-12-22 19:53:19 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,22 @@
 #include "TXLib.h"
+void drawChelPosleSmerti();
 int main()
 {
-  txCreateWindow(1600, 800);
+  txCreateWindow(1680, 1050);
   txSetFillColour(TX_WHITE);
-  txLine(100, 100, 250, 100);
+  drawChelPosleSmerti();
+
+
+
+  return 0;
+}
+
+void drawChelPosleSmerti()
+{
+txLine(100, 100, 250, 100);
   txLine(100, 100, 100, 250);
   txLine(100, 250, 250, 250);
   txLine(250, 250, 250, 100);
   txCircle(200, 150, 20);
   txCircle(150, 150, 20);
-
- 
-
 }
