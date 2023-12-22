7547145ed7bad715a9ed379446646cff61a24867
m0netil <154592830+m0netil@users.noreply.github.com>
2023-12-22 19:48:58 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,14 @@
 int main()
 {
   txCreateWindow(1600, 800);
+  txSetFillColour(TX_WHITE);
+  txLine(100, 100, 250, 100);
+  txLine(100, 100, 100, 250);
+  txLine(100, 250, 250, 250);
+  txLine(250, 250, 250, 100);
+  txCircle(200, 150, 20);
+  txCircle(150, 150, 20);
+
  
 
 }
